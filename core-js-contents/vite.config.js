9c4379bcb174ac040ead18db92d6f80e4a0d6cd0
Saranya Sivasankaran <saranyapsankar@gmail.com>
2025-02-24 13:39:03 +0530

    Update vite.config.js

--- core-js-contents/vite.config.js
+++ core-js-contents/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/jan-explore/core-js-contents/',  // Reflects GitHub Pages path
+  base: '/core-explore/core-js-contents/',  // Reflects GitHub Pages path
   plugins: [react()],
 });
